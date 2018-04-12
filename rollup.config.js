fc2fa2c8f8841249c92a83d0152156e25f7dd7d2
Guido Bouman <guido@q42.nl>
2018-04-12 19:34:24 +0200

    build: Add builds for panelSnap.js

--- rollup.config.js
+++ rollup.config.js
@@ -11,21 +11,41 @@ const banner = `/**
  * LICENSE file in the root directory of this source tree.
  */`;
 
-export default {
-  input: 'src/jquery.panelSnap.js',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
-    { file: pkg.browser, format: 'umd' },
-    { file: 'docs/jquery.panelSnap.js', format: 'umd' },
-  ],
-  plugins: [
-    babel({
-      exclude: ['node_modules/**'],
-    }),
-    uglify(),
-    license({
-      banner,
-    }),
-  ],
-};
+export default [
+  {
+    input: 'src/jquery.panelSnap.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+      { file: pkg.browser, format: 'umd' },
+      { file: 'docs/jquery.panelSnap.js', format: 'umd' },
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+      uglify(),
+      license({
+        banner,
+      }),
+    ],
+  },
+  {
+    input: 'src/panelSnap.js',
+    output: [
+      { file: 'lib/panelSnap.cjs.js', format: 'cjs' },
+      { file: 'lib/panelSnap.esm.js', format: 'es' },
+      { file: 'lib/panelSnap.umd.js', format: 'umd', name: 'panelSnap' },
+      { file: 'docs/panelSnap.js', format: 'umd', name: 'panelSnap' },
+    ],
+    plugins: [
+      babel({
+        exclude: ['node_modules/**'],
+      }),
+      uglify(),
+      license({
+        banner,
+      }),
+    ],
+  },
+];
