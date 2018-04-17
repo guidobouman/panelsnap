aa76466a72f0e7e6ee99beac356232e533ae781b
Guido Bouman <guido@q42.nl>
2018-04-17 13:26:44 +0200

    feat: Add getElementsInContainerViewport utility function

--- src/utilities.js
+++ src/utilities.js
@@ -21,3 +21,27 @@ export function getTargetScrollTop(container, element) {
   const scrollOffset = elementTop - containerTop;
   return scrollOffset + container.scrollTop;
 }
+
+export function getElementsInContainerViewport(container, elementList) {
+  const containerRect = container === document.body ? {
+    top: 0,
+    left: 0,
+    bottom: window.innerHeight,
+    right: window.innerWidth,
+  } : container.getBoundingClientRect();
+
+  return elementList.filter((element) => {
+    const elementRect = element.getBoundingClientRect();
+
+    // Top inside viewport?
+    if (elementRect.top >= containerRect.top && elementRect.top <= containerRect.bottom) {
+      return true;
+    }
+
+    // Bottom inside viewport?
+    if (elementRect.bottom >= containerRect.top && elementRect.bottom <= containerRect.bottom) {
+      return true;
+    }
+    return false;
+  });
+}
