3be0ccb5bf2a0859fb5eee07defc2304ae403c0b
Guido Bouman <guido@q42.nl>
2018-05-14 09:40:33 +0200

    docs: simplify demos and add beter intro
    Event / basic demo & large panel demo have been merged.

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,11 +1,7 @@
 var main = new PanelSnap();
 
-var event_demo = new PanelSnap({
-  container: document.querySelector('.event_demo .panels')
-});
-
-var large_demo = new PanelSnap({
-  container: document.querySelector('.large_demo .panels')
+var default_demo = new PanelSnap({
+  container: document.querySelector('.default_demo .panels')
 });
 
 var horizontal_demo = new PanelSnap({
