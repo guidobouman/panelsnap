547b46e6f04be8b01730af4a1791f6c8bae20fef
Guido Bouman <guido@q42.nl>
2018-05-14 14:32:58 +0200

    docs: refer to separate demos from main docs

--- docs/scripts.js
+++
@@ -1,9 +0,0 @@
-var main = new PanelSnap();
-
-var default_demo = new PanelSnap({
-  container: document.querySelector('.default_demo .panels')
-});
-
-var horizontal_demo = new PanelSnap({
-  container: document.querySelector('.horizontal_demo .panels')
-});
